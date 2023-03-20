5868f6e08c09468421e416d969ea8536ad3e2974
noyamirai <planet1109@hotmail.com>
2023-03-20 10:59:55 +0100

    node base

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('', (req, res) => {
+    res.send('hi');
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
