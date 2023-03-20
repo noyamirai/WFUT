6167473d40184bf34a295a4f4b5c0cc3ac5d0496
noyamirai <planet1109@hotmail.com>
2023-03-20 13:41:45 +0100

    templating and routes

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-import express from 'express';
+const express = require('express');
+const fs = require('fs');
+require('dotenv').config();
+
+// import Fotmob from 'fotmob';
+// const fotmob = new Fotmob();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// SET TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.get('', (req, res) => {
-    res.send('hi');
+app.use('/public', express.static(__dirname + '/public/'));
+app.use('/styles', express.static(__dirname + '/public/styles/'));
+app.use('/images', express.static(__dirname + '/public/images/'));
+
+app.locals.fs = fs;
+
+// ROUTES
+app.use('/', require('./routes/dashboard'));
+
+app.get('*', (req, res) => {
+    // let league = await fotmob.getLeague("9227", "overview", "league", "Netherlands/Amsterdam")
+
+    res.render('layout', {
+        'view': 'blanco',
+        'bodyClass': 'error',
+        'partial': './partials/message', 
+        'messageTitle': 'Oops!', 
+        'messageSubTitle': 'Page not found'
+    });
 })
 
 app.listen(port, () => {
